87c233832c7052b8a79e37e4a7b2debf960928fc
luca-waldvogel <waldvogel.luca@hotmail.com>
2025-05-30 08:17:30 +0200

    deleted comment

--- app.py
+++ app.py
@@ -115,5 +115,5 @@ def open_browser():
     webbrowser.open("http://127.0.0.1:5000/")
 
 if __name__ == "__main__":
-    threading.Timer(1, open_browser).start()  # 1 Sekunde Verzögerung
+    threading.Timer(1, open_browser).start()
     app.run(debug=False)
